0ba2b18034f0716b0fb1a2bbde8e36375c361bbe
Hell Li <hell.li@maxwit.com>
2012-08-24 16:51:09 +0800

    ALSA support
    Signed-off-by: Hell Li <hell.li@maxwit.com>
    Signed-off-by: Cici Cao <cici.cao@maxwit.com>

--- alsa.c
+++ alsa.c
@@ -1,1 +1,85 @@
+#include <alsa/asoundlib.h>
+#include "audio_output.h"
 
+#define DEVICE "hw:0,0"
+
+void *open_alsa(struct mp3_param *param)
+{
+	int ret;
+	snd_pcm_t *pcm;
+	snd_pcm_hw_params_t *hwparams;
+
+	ret = snd_pcm_open(&pcm, DEVICE, SND_PCM_STREAM_PLAYBACK, 0);
+	if (ret < 0) {
+		perror("snd_pcm_open");
+		return NULL;
+	}
+
+	snd_pcm_hw_params_alloca(&hwparams);
+
+	ret = snd_pcm_hw_params_any(pcm, hwparams);
+	if (ret < 0) {
+		perror("snd_pcm_hw_params_any");
+		goto L1;
+	}
+
+	ret = snd_pcm_hw_params_set_access(pcm, hwparams, SND_PCM_ACCESS_RW_INTERLEAVED);
+	if (ret < 0) {
+		perror("snd_pcm_hw_params_set_sccess");
+		goto L1;
+	}
+
+	ret = snd_pcm_hw_params_set_format(pcm, hwparams, SND_PCM_FORMAT_S16_LE);
+	if (ret < 0) {
+		perror("snd_pcm_hw_params_set_format");
+		goto L1;
+	}
+
+	ret = snd_pcm_hw_params_set_channels(pcm, hwparams, param->channels);
+	if (ret < 0) {
+		perror("snd_pcm_hw_params_set_channels");
+		goto L1;
+	}
+
+	ret = snd_pcm_hw_params_set_rate_near(pcm, hwparams, (unsigned int *)&param->rate, NULL);
+	if (ret < 0) {
+		perror("snd_pcm_hw_params_set_rate_near");
+		goto L1;
+	}
+
+	ret = snd_pcm_hw_params(pcm, hwparams);
+	if (ret < 0) {
+		perror("snd_pcm_hw_params");
+		goto L1;
+	}
+
+	ret = snd_pcm_prepare(pcm);
+	if (ret < 0) {
+		perror("snd_pcm_prepare");
+		goto L1;
+	}
+
+	return pcm;
+
+L1:
+	snd_pcm_close(pcm);
+
+	return NULL;
+}
+
+int close_alsa(snd_pcm_t *pcm)
+{
+	snd_pcm_close(pcm);
+
+	return 0;
+}
+
+int play_alsa_frames(snd_pcm_t *pcm, u8 *raw_buff, int frames)
+{
+
+	snd_pcm_mmap_writei(pcm, raw_buff, frames);
+
+	snd_pcm_drain(pcm);
+
+	return 0;
+}
--- audio_output.c
+++ audio_output.c
@@ -1,44 +1,61 @@
 #include "audio_output.h"
+#include "alsa.h"
+#include <stdlib.h>
 
-struct audio_output *open_audio(audio_dev_t type, struct mp3_param *param)
-{
-	// fixme!
-//	struct audio_output *out;
-//
-//	out = malloc(sizeof(*out));
-//	if (NULL == out) {
-//		return NULL;
-//	}
-//
-//	out->type = type;
-//
-//	if (type == AUDIO_ALSA) {
-//		out->outdev = open_alsa();
-//	} else if (type == AUDIO_GSTREAMER) {
-//		out->outdev = open_gstreamer();
-//	}
+struct audio_output *open_audio(audio_dev_t type, struct mp3_param *param) {
+	struct audio_output *out;
 
-	return 0;
+	out = malloc(sizeof(*out));
+	if (NULL == out) {
+		return NULL;
+	}
+
+	out->type = type;
+
+	switch (type) {
+	case AUDIO_GSTREAMER:
+		// out->outdev = open_gstreamer(param); fixme!
+		break;
+
+	case AUDIO_ALSA:
+		out->outdev = open_alsa(param);
+		break;
+	}
+
+	return out;
 }
 
-int close_audio(struct audio_output *out)
-{
-	// fixme!
-//	if (out->type == AUDIO_ALSA) {
-//		...
-//	} else ...
+int close_audio(struct audio_output *out) {
+	switch (out->type) {
+	case AUDIO_GSTREAMER:
+		// close_gstreamer(out);
+		break;
+
+	case AUDIO_ALSA:
+		close_alsa(out->outdev);
+		break;
+	}
+
+	free(out);
 
 	return 0;
 }
 
 int play_frames(struct audio_output *out, u8 *raw_buff, size_t size, struct mp3_param *param, u8 *lrc, size_t lrc_size)
 {
-	// fixme!
-//	if (out->type == AUDIO_ALSA) {
-//		alsa_play_frames(out->outdev, raw_buff, size, param);
-//	} else if (out->type == AUDIO_GSTREAMER) {
-//		gstreamer_play_frames(out->outdev, raw_buff, size, param);
-//	}
+	int frames;
+
+	frames = size / (param->channels * param->bits_per_sample / 8);
+
+	switch (out->type) {
+	case AUDIO_GSTREAMER:
+		// play_gstreamer_frames(out, raw_buff, frames); fixme!
+		break;
+
+	case AUDIO_ALSA:
+		play_alsa_frames(out, raw_buff, frames);
+		break;
+	}
 
 	return 0;
 }
